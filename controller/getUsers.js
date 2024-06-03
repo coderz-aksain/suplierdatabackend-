dd301e769f514009b7c774adaed81aae69688fc6
coderz-aksain <ayushnikhil2002@gmail.com>
2024-06-03 16:24:38 +0530

    Maximum no of Payment terms is done with id is done

--- controller/getUsers.js
+++ controller/getUsers.js
@@ -1,32 +1,37 @@
-// const User = require("../models/File");
-// import File from "../models/File";
-// const File=require('../models/File')
-// exports.getUser = async (req, res) => {
-// 	try {
-// 		const  userData2 = File.count({paymentterms})
-// 		const userData = await File.find({});
-// 		res.json({ success: true, data: userData,userData2 });
-// 	} catch (error) {
-// 		res.status(500).json({ success: false, error: error });
-// 	}
-// };
-
-
 const File = require('../models/File');
 
 exports.getUser = async (req, res) => {
 	try {
-		// Count the number of documents with paymentterms equal to 'paymentterm90'
-		// const userData2 = await File.find.count({ paymentterm: 'Net 90' });
-		const userData2 = await File.countDocuments({ paymentterm: 'Net 90' });
+		// Aggregate documents to count the number of each unique payment term
+		const paymentTermCounts = await File.aggregate([
+			{
+				$group: {
+					_id: "$paymentterm",
+					count: { $sum: 1 }
+				}
+			},
+			{
+				$sort: { count: -1 }
+			},
+			{
+				$limit: 1
+			}
+		]);
+		console.log(paymentTermCounts);
 
-		console.log(userData2);
 		// Retrieve all documents from the collection
 		const userData = await File.find({});
-		// console.log(userData);
-		// Send the response with the retrieved documents and the count
-		res.json({ success: true, data: userData, count: userData2 });
-		
+
+		// Prepare the response data
+		const responseData = {
+			success: true,
+			data: userData,
+			topPaymentTerm: paymentTermCounts.length > 0 ? paymentTermCounts[0] : null
+		};
+
+		// Send the response with the retrieved documents and the top payment term count
+		res.json(responseData);
+
 	} catch (error) {
 		// Handle any errors that occurred during the process
 		res.status(500).json({ success: false, error: error.message });
